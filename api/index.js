070206a63e9a117ef61c60a18c5e9a15f5b45a93
Kuba Jastrzębski <kuba.jastrz@outlook.com>
2019-08-19 22:13:38 +0200

    Fix heroku port

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(helmet());
 app.use(cors());
@@ -11,6 +12,6 @@ app.get('/ping', (req, res) => {
     res.status(200).send('pong');
 });
 
-app.listen('3000', () => {
-    console.log('Ping me at localhost:3000');
+app.listen('PORT', () => {
+    console.log(`Ping me at localhost:${PORT}`);
 });
